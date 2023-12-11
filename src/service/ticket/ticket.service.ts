785b5553d90f709ab311f675debb830789521907
geofannyher <geofannyher@gmail.com>
2023-12-11 13:46:51 +0700

    feat: react query

--- src/service/ticket/ticket.service.ts
+++ src/service/ticket/ticket.service.ts
@@ -1,11 +1,28 @@
+import {
+  IFindTicketProps,
+  ITicketDetail,
+} from "@/interface/ticket/ticket.interface";
 import { privateApi } from "@/shared/axios/axios";
-import { TTicketProps } from "@/types/ticket/ticket.types";
+import { useQuery } from "@tanstack/react-query";
 
-export const getTiket = async ({ id }: TTicketProps) => {
-  try {
-    const response = await privateApi.get(`/tiket/${id}`);
-    return response.data.data;
-  } catch (error: any) {
-    return error.response;
-  }
+export const QfindTicketbyId = ({ id }: IFindTicketProps) => {
+  const fetcher = () => privateApi.get(`/tiket/${id}`);
+  return useQuery({
+    queryKey: ["ticketId"],
+    queryFn: fetcher,
+    select(res): ITicketDetail {
+      return res?.data?.data;
+    },
+  });
+};
+
+export const QfindTicket = () => {
+  const fetcher = () => privateApi.get(`/tiket`);
+  return useQuery({
+    queryKey: ["ticket"],
+    queryFn: fetcher,
+    select(res) {
+      return res?.data?.data;
+    },
+  });
 };

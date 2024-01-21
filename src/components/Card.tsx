8477012121722704369e99b59ffc9224c04b1ee0
geofannyher <geofannyher@gmail.com>
2024-01-21 21:50:59 +0700

    fix: edit,riwayat

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,41 +10,37 @@ const Card = ({
   tags,
 }: ICardProps) => {
   return (
-    <>
-      <Link to={`/detail/${id}`}>
-        <div className="flex w-auto cursor-pointer flex-col">
-          <div className="truncate rounded-xl shadow-lg">
-            <div className="relative h-64 w-full">
-              <img
-                src={image}
-                className="h-full w-full object-cover"
-                alt={`Image`}
-              />
-              <div className="absolute right-0 top-0">
-                <div className="flex w-20 justify-center rounded-bl-md bg-mainColors text-sm font-semibold text-white">
-                  {tags}
-                </div>
+    <Link to={`/detail/${id}`}>
+      <div className="flex w-auto cursor-pointer flex-col">
+        <div className="truncate rounded-xl shadow-lg">
+          <div className="relative h-64 w-full">
+            <img
+              src={image}
+              className="h-full w-full object-cover"
+              alt={`Image`}
+            />
+            <div className="absolute right-0 top-0">
+              <div className="flex w-20 justify-center rounded-bl-md bg-mainColors text-sm font-semibold text-white">
+                {tags}
               </div>
             </div>
-            <h1 className="px-4 pt-5 text-sm font-bold text-mainColors lg:font-bold">
-              {title}
-            </h1>
-            <p className="px-4 pt-3 text-[12px] font-medium  text-gray-500 lg:text-[14px]">
-              {date}
-            </p>
-            <div className="flex flex-row gap-2 px-4 py-1">
-              <p className="text-xs font-semibold lg:text-base">{price}</p>
-            </div>
-            <div className="px-2 py-4">
-              <hr />
-            </div>
-            <p className="px-4 pb-4 text-xs font-bold lg:text-base">
-              {location}
-            </p>
           </div>
+          <h1 className="px-4 pt-5 text-sm font-bold text-mainColors lg:font-bold">
+            {title}
+          </h1>
+          <p className="px-4 pt-3 text-[12px] font-medium  text-gray-500 lg:text-[14px]">
+            {date}
+          </p>
+          <div className="flex flex-row gap-2 px-4 py-1">
+            <p className="text-xs font-semibold lg:text-base">{price}</p>
+          </div>
+          <div className="px-2 py-4">
+            <hr />
+          </div>
+          <p className="px-4 pb-4 text-xs font-bold lg:text-base">{location}</p>
         </div>
-      </Link>
-    </>
+      </div>
+    </Link>
   );
 };
 
